b52651e36cecdc2275b2255a2f828e2a2951aa9a
Saiful Islam Apu <132099577+saifulapu@users.noreply.github.com>
2023-06-06 02:46:44 -0500

    Update embeddedMarkovModel.m

--- embeddedMarkovModel.m
+++ embeddedMarkovModel.m
@@ -1,41 +1,60 @@
-function [transitionMatrix, simulatedFacies] = embeddedMarkovModel(faciesData)
-% input- faciesData: facies data
-%output- tm: transition matrix;
-%        sf: simulated facies
+%Load CrameriColourMaps7.0.mat for the colormap
 
-  % Create the transition matrix.
-  transitionMatrix = transitionMatrix(faciesData);
+%% Facies Transition Proabibilty
+FaciesTransitionProb = calcFTProb('FC.csv'); %change the facies accordingly in the calcFTProb.m function
+%% Stationary distribution analysis
+stationary_distribution = calcStationaryDist(FaciesTransitionProb);
+%disp(stationary_distribution);
+%uncomment for a simple bar chart if necessary
+%facies = {'St', 'Htb (Sfl/Fle/Sw)', 'Sp', 'S (Sch/Sb/Sm)', 'F(Fm/Fst)'}; % adjust this to match your actual facies
+%figure;
+%bar(stationary_distribution);
+%title('Stationary Distribution of Facies');
+%xlabel('Facies');
+%ylabel('Stationary Distribution');
+%xticks(1:length(facies));
+%xticklabels(facies);
 
-  % Simulate facies change.
-  numSteps = 100;
-  simulatedFacies = simulateFaciesChange(transitionMatrix, numSteps);
+%% Markov Model Chain computation
+% Define initial state
+initial_state = 1;
+% Define sequence length
+sequence_length = 100;
+% Generate sequence
+sequence = FaciesMarkovChainSim(FaciesTransitionProb, initial_state, sequence_length);
+% Display sequence
+%disp(sequence);
 
+%% Markov Model chain simulation (bar)
+% Define facies
+facies = {'St', 'Htb (Sfl/Fle/Sw)', 'Sp', 'S (Sch/Sb/Sm)', 'F(Fm/Fst)'}; 
+
+% Create figure
+figure;
+
+for i = 1:5
+    % Define initial state
+    initial_state = i;
+
+    % Generate sequence
+    sequence = FaciesMarkovChainSim(transition_probabilities, initial_state, sequence_length);
+
+    % Calculate frequency of each state
+    [counts, ~] = hist(sequence, 1:length(facies));
+
+    % Create subplot
+    subplot(5, 1, i);
+
+    % Create bar chart
+    bar(counts);
+
+    % Set x-tick labels to facies
+    xticklabels(facies);
+
+    % Set title
+    title(sprintf('Simulation %d', i*30));
 end
 
-function transitionMatrix = transitionMatrix(faciesData)
+%% Markov Model chain simulation (matrix)
+FaciesMMSimulations(FaciesTransitionProb, facies, 1000, 100, 6);
 
-  % Count the number of times each pair of facies occur together.
-  transitionCounts = accumarray(faciesData, faciesData);
-
-  % Create the transition matrix.
-  transitionMatrix = transitionCounts ./ sum(transitionCounts, 2);
-
-end
-
-function simulatedFacies = simulateFaciesChange(transitionMatrix, numSteps)
-
-  % Initialize the simulated facies.
-  simulatedFacies = zeros(numSteps, 1);
-
-  % Simulate facies change for the specified number of steps.
-  for i = 1:numSteps
-
-    % Generate a random number.
-    randomNumber = rand;
-
-    % Select the next facies.
-    simulatedFacies(i) = find(transitionMatrix(simulatedFacies(i - 1), :) > randomNumber);
-
-  end
-
-end
