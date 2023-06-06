ccd364aa3d1d3507ad2f3632ea72eaaa491f9a50
Saiful Islam Apu <132099577+saifulapu@users.noreply.github.com>
2023-06-06 02:58:03 -0500

    Update embeddedMarkovModel.m

--- embeddedMarkovModel.m
+++ embeddedMarkovModel.m
@@ -1,5 +1,4 @@
 %Load CrameriColourMaps7.0.mat for the colormap
-
 %% Facies Transition Proabibilty
 FaciesTransitionProb = calcFTProb('FC.csv'); %change the facies accordingly in the calcFTProb.m function
 %% Stationary distribution analysis
@@ -14,6 +13,8 @@ stationary_distribution = calcStationaryDist(FaciesTransitionProb);
 %ylabel('Stationary Distribution');
 %xticks(1:length(facies));
 %xticklabels(facies);
+%% Transitional diagram
+FaciesTransitionDiagram(FaciesTransitionProb, facies);
 
 %% Markov Model Chain computation
 % Define initial state
@@ -28,33 +29,27 @@ sequence = FaciesMarkovChainSim(FaciesTransitionProb, initial_state, sequence_le
 %% Markov Model chain simulation (bar)
 % Define facies
 facies = {'St', 'Htb (Sfl/Fle/Sw)', 'Sp', 'S (Sch/Sb/Sm)', 'F(Fm/Fst)'}; 
-
 % Create figure
 figure;
-
 for i = 1:5
     % Define initial state
     initial_state = i;
-
     % Generate sequence
     sequence = FaciesMarkovChainSim(transition_probabilities, initial_state, sequence_length);
-
     % Calculate frequency of each state
     [counts, ~] = hist(sequence, 1:length(facies));
-
     % Create subplot
     subplot(5, 1, i);
-
     % Create bar chart
     bar(counts);
-
     % Set x-tick labels to facies
     xticklabels(facies);
-
     % Set title
     title(sprintf('Simulation %d', i*30));
 end
 
 %% Markov Model chain simulation (matrix)
 FaciesMMSimulations(FaciesTransitionProb, facies, 1000, 100, 6);
+
+
 
